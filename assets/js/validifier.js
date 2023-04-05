83b8ebf5bdad282a820e8e38e57fe81066bcef0e
Jeet <56454817+jedetin@users.noreply.github.com>
2023-04-05 15:48:11 +0530

    added file upload
    modified decrypt function

--- assets/js/validifier.js
+++ assets/js/validifier.js
@@ -14,8 +14,10 @@ const decData = {
 }
 
 const userKey = {
-    "private": "",
-    "public": "",
+    "public": {
+        "id": "",
+        "string": ""
+    },
 }
 function print(string, color) {
     return console.log('%c' + string, 'color: ' + color + ';')
@@ -39,7 +41,10 @@ let generate = () => {
 
     var SamsRSAkey = cryptico.generateRSAKey(serverKey, data.bits);
     var SamsPublicKeyString = cryptico.publicKeyString(SamsRSAkey);
+
+    userKey.public.string = SamsPublicKeyString;
     var SamsPublicKeyId = cryptico.publicKeyID(SamsPublicKeyString)
+    userKey.public.id = SamsPublicKeyId;
 
     var MattsRSAkey = cryptico.generateRSAKey(data.text.pass, data.bits);
     var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);
@@ -62,14 +67,16 @@ let generate = () => {
     let divar = `
     <label class="form-label">Key ID</label>
     <input type="text" class="form-control mb-5" readonly value="${SamsPublicKeyId}">
+    <label class="form-label">Key String</label>    <small class='text-muted'>This will be used for verification</small>
+    <input type="text" class="form-control mb-5" readonly value="${SamsPublicKeyString}">\
 
-    <input type="text" class="form-control mb-5" readonly value="${SamsPublicKeyString}">
     <label class="form-label">Encrypted Text</label>
 
         <textarea rows="5" id="resultgen" class="form-control" readonly>${EncryptionResult.cipher}</textarea>
     `;
     document.getElementById("returnDiv").innerHTML = divar;
-    //  return EncryptionResult.cipher;
+    decData.cipher = EncryptionResult.cipher;
+    return EncryptionResult.cipher;
 }
 
 function verify(code) {
@@ -80,6 +87,12 @@ function verify(code) {
         return `<i class='fas fa-alert></i> Forged`
     }
 }
+
+// let compKey = () => {
+
+
+// }
+
 function decrypt() {
     const bits = 512
     decData.pass = document.getElementById('passphrase').value;
@@ -91,18 +104,49 @@ function decrypt() {
     var MattsPublicKeyId = cryptico.publicKeyID(MattsPublicKeyString)
 
     var DecryptionResult = cryptico.decrypt(decData.cipher, MattsRSAkey);
-
-    print("The decrypted message:");
-    print(DecryptionResult.plaintext);
-    print("DecryptionResult.signature: " + DecryptionResult.signature);
+    if (decData.id != null) {
+        if (DecryptionResult.publicKeyString == decData.id) {
+            console.log("true");
+        } else {
+            console.log("false");
+        }
+    } else {
+        console.log("No Key ID provided");
+    }
     let divar = `
     <span class="badge bg-primary">${DecryptionResult.signature}</span>
     <label class="form-label fw-bolder">Decrypted Text:</label>
-    ${DecryptionResult.plaintext}  <hr/>
-    
-    ${DecryptionResult.publicKeyString}
-
-`;
-
+    ${DecryptionResult.plaintext}  <hr/>`;
     document.getElementById("returnDiv").innerHTML = divar;
+}
+
+let saveFile = () => {
+    let data = `${decData.cipher}`
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = userKey.public.id + '.txt';
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click();
+}
+function loadFileAsText() {
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+    var namekeyId = fileToLoad.name
+    namekeyId = namekeyId.replace(".txt", "")
+    var fileReader = new FileReader();
+    fileReader.onload = function (fileLoadedEvent) {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("ciphertext").value = textFromFileLoaded;
+        document.getElementById("keyid").value = namekeyId;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
 }
